cdb672dd4b0ce26a5d4943ce7307565a1da2bffd
Estefan Hu <estefanhu074@gmail.com>
2020-06-10 22:44:56 -0700

    added jsonwebtokens

--- routes/mobileRoutes.js
+++ routes/mobileRoutes.js
@@ -1,4 +1,9 @@
 const router = require('express').Router();
+const mongoose = require('mongoose');
+const requireAuth = require('../middlewares/requireAuth');
+const Story = mongoose.model('Story');
+
+router.use(requireAuth);
 
 router.get('/', async (req, res) => {
   try {
@@ -9,11 +14,11 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/logout', (req, res) => {
+router.get('/story', async (req, res) => {
   try {
-    res.json({ msg: 'Loging out' });
-  } catch (err) {
-    res.status(500).json({ err });
+    res.json({msg: 'Hello World'});
+  } catch (error) {
+    res.status(422).send({ error: error });
   }
 })
 
