cdd6cdb1fecc1acb68221c2197b921a9a8de494d
Estefan Hu <estefanhu074@gmail.com>
2020-06-20 21:13:59 -0700

    removed redis

--- app.js
+++ app.js
@@ -27,10 +27,7 @@ mongoose.connection.once('open', () => {
 require('dotenv').config();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true,
-}));
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.json({ msg: 'hello wolrd' });
