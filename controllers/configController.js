9ac3eb8442d3623bee39810e257a6437c33a4630
Estefan Hu <estefanhu074@gmail.com>
2020-06-24 06:44:21 -0700

    corrected async

--- controllers/configController.js
+++ controllers/configController.js
@@ -1,8 +1,10 @@
-const catchAsync = require('./../utils/catchAsync.js');
-
-exports.getMapKey = catchAsync((_, res, next) => {
-  res.status(200).json({
-    status: 'success',
-    payload: process.env.MAPBOX_ACCESS_TOKEN
-  });
-});
+exports.getMapKey = (_, res) => {
+  try {
+    res.status(200).json({
+      status: 'success',
+      payload: process.env.MAPBOX_ACCESS_TOKEN
+    });
+  } catch (error) {
+    res.status(500).json({ status: 'failure', payload: error });
+  }
+};
