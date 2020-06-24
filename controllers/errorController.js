b7667a432cdddc0424632b0120ccffda2f46c90c
Estefan Hu <estefanhu074@gmail.com>
2020-06-24 08:01:19 -0700

    added validator

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -11,6 +11,12 @@ const handleDuplicateFieldsDB = err => {
   return new AppError(message, 400);
 }
 
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+}
+
 const sendErrorDev = (error, res) => {
   res.status(error.statusCode).json({
     status: error.status,
@@ -50,6 +56,7 @@ module.exports = (error, req, res, next) => {
     err = Object.assign(err, error);
     if (err.name === 'CastError') err = handleCastErrorDB(err);
     if (err.code === 11000) err = handleDuplicateFieldsDB(err);
+    if (err.name === 'ValidationError') err = handleValidationErrorDB(err);
 
     sendErrorProd(err, res);
   }
