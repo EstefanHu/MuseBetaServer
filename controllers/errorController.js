032f12253418be78cd8e7b4043888a2a459cac7d
Estefan Hu <estefanhu074@gmail.com>
2020-06-24 07:07:03 -0700

    extracted error logic

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,38 @@
+const sendErrorDev = (error, res) => {
+  res.status(error.statusCode).json({
+    status: error.status,
+    error: error,
+    payload: error.message,
+    stack: error.stack
+  });
+};
+
+const sendErrorProd = (error, res) => {
+  if (error.isOperational) {
+    res
+      .status(error.statusCode)
+      .json({
+        status: error.status,
+        payload: error.message,
+      })
+  } else {
+    console.error('Error: ', err);
+    res
+      .status(500)
+      .json({
+        status: 'error',
+        payload: 'Something went wrong with the server.'
+      });
+  };
+};
+
 module.exports = (error, req, res, next) => {
   error.statusCode = error.statusCode || 500;
   error.status = error.status || 'error';
 
-  res.status(error.statusCode).json({
-    type: error.status,
-    payload: error.message
-  });
-}
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(error, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(error, res);
+  }
+};
