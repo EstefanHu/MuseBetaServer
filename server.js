4f1e6f3889c645473083f00423d56ac2f9c4258d
Estefan Hu <estefanhu074@gmail.com>
2020-06-24 08:14:30 -0700

    added unhandledRejection

--- server.js
+++ server.js
@@ -15,11 +15,15 @@ mongoose.connect(`mongodb://localhost/${DB_CONNECTION}`,
   });
 mongoose.connection.once('open', () => {
   console.log(`connection has been established to ${DB_CONNECTION}`);
-}).on('err', err => {
-  console.log('Connection Error: ' + err);
+}).on('err', error => {
+  console.error('Connection Error: ' + error);
 });
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
+process.on('unhandledRejection', error => {
+  console.error(error.name, error.message);
+})
