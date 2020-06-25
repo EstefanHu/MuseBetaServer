2856c13edb59d7fbc33fad07ffc9b6160c83214f
Estefan Hu <estefanhu074@gmail.com>
2020-06-24 22:00:42 -0700

    installed router

---
+++ routes/monumentRoute.js
@@ -0,0 +1,19 @@
+const monumentController = require('./../controllers/monumentController.js');
+const authController = require('./../controllers/authController.js');
+const router = express.Router();
+
+router
+  .route('/')
+  .get(authController.protect, monumentController.getMonumnets)
+  .post(authController.protect, monumentController.createMonument);
+
+router
+  .route('/:id')
+  .get(authController.protect, monumentController.getMonumnet)
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin'),
+    monumentController.deleteMonument
+  );
+
+module.exports = router;
