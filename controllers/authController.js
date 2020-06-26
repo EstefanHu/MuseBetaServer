69719345b9d853e42f8b177a30575d586b16095b
Estefan Hu <estefanhu074@gmail.com>
2020-06-25 17:55:40 -0700

    added cookie based jwt token

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,6 +12,17 @@ const createSendToken = (user, statusCode, res) => {
     process.env.JWT_KEY,
     { expiresIn: process.env.JWT_EXPIRES_IN }
   );
+  const cookieOptions = {
+    expires: new Date(Date.now()
+      + process.env.JWT_COOKIE_EXPIRES_IN
+      * 24 * 60 * 60 * 1000),
+    httpOnly: true
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+
+  user.password = undefined;
+
   res
     .status(statusCode)
     .json({
@@ -19,7 +30,7 @@ const createSendToken = (user, statusCode, res) => {
       token,
       payload: user
     });
-}
+};
 
 exports.register = catchAsync(async (req, res, next) => {
   let newUser = await User.create({
